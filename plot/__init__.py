bea0164330b3c64b60e872ee10afbd8d0b4038a0
LIU SHUANG <mikufreedom@gmail.com>
2018-09-25 12:10:17 +0900

    change name

--- plot/__init__.py
+++ plot/__init__.py
@@ -1,8 +1,11 @@
-from . import staticPlot
-from . import interactPlot
-
-import plotly.plotly as py
-import plotly.graph_objs as go
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+# from . import staticPlot
+# from . import interactPlot
+#
+# import plotly.plotly as py
+# import plotly.graph_objs as go
+# from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
 # init_notebook_mode()
+
+# from interactPlot import *
+# from staticPlot import *
--- plot/interactPlot.py
+++ plot/interactPlot.py
@@ -7,9 +7,9 @@ init_notebook_mode()
 def plotOneLine(x,y,mode='lines+markers',legend_name='',title=''):
     d=[go.Scatter(x=x,y=y,mode=mode,name=legend_name)]
     fig=go.Figure(data=d,layout=go.Layout(title=title))
-    iplot(fig,show_link=True)
+    iplot(fig,show_link=False)
 
-def plotMultiDataFrameColumn(data_frame,mode_dict={},title=''):
+def plotDataFrameColumn(data_frame,mode_dict={},title=''):
     # the chage_mode_dict is a dict to change style of column eg. {'col1':'lines+markers','col2':'lines'}
     assert(isinstance(data_frame, pd.core.frame.DataFrame) == True),"Input should be Pands DataFrame"
     d=[]
@@ -17,4 +17,4 @@ def plotMultiDataFrameColumn(data_frame,mode_dict={},title=''):
         mode = mode_dict.get(key,'lines+markers')
         d.append(go.Scatter(x=series.index,y=series.values,name=key,mode=mode))
     fig=go.Figure(data=d,layout=go.Layout(title=title))
-    iplot(fig,show_link=True)
+    iplot(fig,show_link=False)
